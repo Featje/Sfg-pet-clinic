10895098b4972864650b3f88d15d20ef07b28eec
Vincent <vincentsterrenburg50@hotmail.com>
2019-12-02 19:31:19 +0100

    adding POJOs Closes #2.

---
+++ src/main/java/com/distinguo/sfgpetclinic/model/Owner.java
@@ -0,0 +1,6 @@
+package com.distinguo.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+
+}
---
+++ src/main/java/com/distinguo/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.distinguo.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
