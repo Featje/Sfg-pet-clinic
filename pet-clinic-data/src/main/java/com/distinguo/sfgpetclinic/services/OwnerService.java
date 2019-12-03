aa156518980a7a27a79b22267fdc18bee7f5e8fd
Vincent <vincentsterrenburg50@hotmail.com>
2019-12-03 20:51:34 +0100

    Refactor services interfaces to common crud service #11.

--- pet-clinic-data/src/main/java/com/distinguo/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/distinguo/sfgpetclinic/services/OwnerService.java
@@ -2,13 +2,7 @@ package com.distinguo.sfgpetclinic.services;
 
 import com.distinguo.sfgpetclinic.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
-    Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findall();
-
-
+public interface OwnerService extends CrudService<Owner, Long>{
+    Owner findByLastName (String lastName);
 }
--- pet-clinic-data/src/main/java/com/distinguo/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/distinguo/sfgpetclinic/services/VetService.java
@@ -2,11 +2,6 @@ package com.distinguo.sfgpetclinic.services;
 
 import com.distinguo.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findall();
+public interface VetService extends CrudService<Vet,Long> {
 
 }
