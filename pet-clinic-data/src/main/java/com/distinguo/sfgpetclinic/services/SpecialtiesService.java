46385813173a4faece98eee03c689dc80dc07b44
Vincent <vincentsterrenburg50@hotmail.com>
2019-12-13 10:21:07 +0100

    Create speciality map service impl Closes #32.

---
+++ pet-clinic-data/src/main/java/com/distinguo/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.distinguo.sfgpetclinic.services;
+
+import com.distinguo.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long> {
+
+}
