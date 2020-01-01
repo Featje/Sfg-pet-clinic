1ea382360c09e53e0a4b0dc9ca65575ca87844e1
Vincent <vincentsterrenburg50@hotmail.com>
2020-01-01 16:05:13 +0100

    Created mapservice for visit Closes #47

---
+++ pet-clinic-data/src/main/java/com/distinguo/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.distinguo.sfgpetclinic.services;
+
+import com.distinguo.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/distinguo/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,38 @@
+package com.distinguo.sfgpetclinic.services.map;
+
+import com.distinguo.sfgpetclinic.model.Visit;
+import com.distinguo.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit,Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findByID(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null || visit.getPet().getOwner().getId()== null){
+        throw new RuntimeException("Invalid visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+    super.delete(visit);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+    super.deleteById(id);
+    }
+}
