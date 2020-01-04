f8162feb110c9d05e374f208854a100dab94e4b4
Vincent <vincentsterrenburg50@hotmail.com>
2020-01-04 12:29:21 +0100

    Add project Lombok and refactor  Closes #52

--- pet-clinic-data/src/main/java/com/distinguo/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/distinguo/sfgpetclinic/model/PetType.java
@@ -1,20 +1,19 @@
 package com.distinguo.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
     @Column(name = "name")
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
